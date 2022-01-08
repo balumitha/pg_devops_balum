11b3b0304e5b0a0ce6af413018aa6a42d4fb7b2c
balum <balum@demao.local>
2022-01-08 16:44:38 +0000

    lastupdate

--- balu.java
+++ balu.java
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 
 
 this is my first java code
 
 new change as per customer requirement 
 completed code for new demo
+=======
+this is my first java code
+>>>>>>> parent of 96b2132... second commit
