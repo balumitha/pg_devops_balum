9fdf733d26096e04b69f953360cc234dd103ba43
balum <balum@demao.local>
2022-01-08 17:03:07 +0000

    Revert "lastupdate"
    This reverts commit 11b3b0304e5b0a0ce6af413018aa6a42d4fb7b2c.

--- balu.java
+++ balu.java
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 
 
 this is my first java code
 
 new change as per customer requirement 
 completed code for new demo
-=======
-this is my first java code
->>>>>>> parent of 96b2132... second commit
